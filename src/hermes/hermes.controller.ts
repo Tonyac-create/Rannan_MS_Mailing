0e525aeec0e25e0084110399091a27fc4b68f547
Tonyac-create <angelique.copere@gmail.com>
2024-04-11 21:03:18 +0200

    update with my sendgrid account

--- src/hermes/hermes.controller.ts
+++ src/hermes/hermes.controller.ts
@@ -5,21 +5,21 @@ import * as fs from 'fs';
 
 @Controller()
 export class HermesController {
-  constructor(private readonly hermesService: HermesService) {}
+  constructor(private readonly hermesService: HermesService) { }
 
   @MessagePattern("validationMail")
-  async registerMail(@Payload() data: {email: string, nickname: string, token: string}) {
-  // Lire le contenu du fichier "resetTemplate.html"
+  async registerMail(@Payload() data: { email: string, nickname: string, token: string }) {
+    // Lire le contenu du fichier "registerTemplate.html"
     const templatePath = './src/hermes/template/registerTemplate.html';
     let templateContent = fs.readFileSync(templatePath, 'utf8');
-  // Remplacer le marqueur de substitution par la valeur de 'url'
-    console.log("URL :", `http://localhost:5173/mail/return/validation/${data.token}`)
+    // Remplacer le marqueur de substitution par la valeur de 'url'
+    // console.log("URL :", `http://localhost:5173/mail/return/validation/${data.token}`)
     templateContent = templateContent.replace('{{URL_PLACEHOLDER}}', `http://localhost:5173/mail/return/validation/${data.token}`);
     templateContent = templateContent.replace('{{NICKNAME}}', data.nickname);
-  // Créer l'objet de message pour SendGrid avec le contenu modifié du template
+    // Créer l'objet de message pour SendGrid avec le contenu modifié du template
     const msg = {
       to: data.email,
-      from: 'pilath.thomas@ikmail.com', // email vérifié sur le compte SendGrid
+      from: 'erepocangele@gmail.com', // email vérifié sur le compte SendGrid
       subject: 'Validation de votre inscription sur le site Rannan.io',
       text: `Validation de votre inscription sur le site Rannan.io`,
       html: templateContent,
@@ -27,19 +27,19 @@ export class HermesController {
     this.hermesService.sendMail(msg);
   }
 
-@MessagePattern("resetMail")
-  async resetMail(@Payload() data: {email: string, nickname: string, token: string}) {
-  // Lire le contenu du fichier "resetTemplate.html"
+  @MessagePattern("resetMail")
+  async resetMail(@Payload() data: { email: string, nickname: string, token: string }) {
+    // Lire le contenu du fichier "resetTemplate.html"
     const templatePath = './src/hermes/template/resetTemplate.html';
     let templateContent = fs.readFileSync(templatePath, 'utf8');
-  // Remplacer le marqueur de substitution par la valeur de 'url'
-    console.log("URL :", `http://localhost:5173/mail/return/reset/${data.token}`)
+    // Remplacer le marqueur de substitution par la valeur de 'url'
+    // console.log("URL :", `http://localhost:5173/mail/return/reset/${data.token}`)
     templateContent = templateContent.replace('{{URL_PLACEHOLDER}}', `http://localhost:5173/mail/return/reset/${data.token}`);
     templateContent = templateContent.replace('{{NICKNAME}}', data.nickname);
-  // Créer l'objet de message pour SendGrid avec le contenu modifié du template
+    // Créer l'objet de message pour SendGrid avec le contenu modifié du template
     const msg = {
       to: data.email,
-      from: 'pilath.thomas@ikmail.com', // email vérifié sur le compte SendGrid
+      from: 'erepocangele@gmail.com', // email vérifié sur le compte SendGrid
       subject: 'Demande de re initialisation du mot de passe',
       text: `Demande de re initialisation du mot de passe`,
       html: templateContent,
--- src/hermes/hermes.service.ts
+++ src/hermes/hermes.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@nestjs/common';
-const sgMail = require('@sendgrid/mail');
+// import sgMail from '@sendgrid/mail';
+import * as sgMail from '@sendgrid/mail';
+
+import { config } from "dotenv"
+import { join } from 'path';
+config({ path: join(__dirname, '../../.env') })
 
 @Injectable()
 export class HermesService {
 
   async sendMail(msg: any): Promise<any> {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
     try {
-      await sgMail.send(msg);
+      await sgMail.send(msg)
     } catch (error) {
-      console.error(error);
+      console.error(error)
       if (error.response) {
+        console.log("erreur venant du service nestjs hermes");
         console.error(error.response.body)
       }
     }
